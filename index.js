41f5baffc7b33fe3fcfb376eb21d0eff8df66cf4
Bryan Arbelo <magestican.visualkei@gmail.com>
2017-08-24 00:17:53 +1000

    UPDATED INDEX

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
-'use strict';
+require('../dist/bundle.js');
 
-module.exports = require('./src/main.js');
+var validMe = global.ValidMe;
+var clearAllValidationErrors = global.clearAllValidationErrors
+var forceValidation = global.forceValidation
+
+module.exports = {validMe,clearAllValidationErrors,forceValidation};
--- src/main.js
+++ src/main.js
@@ -1,7 +1,1 @@
-require('../dist/bundle.js');
-
-var validMe = global.ValidMe;
-var clearAllValidationErrors = global.clearAllValidationErrors
-var forceValidation = global.forceValidation
-
-module.exports = {validMe,clearAllValidationErrors,forceValidation};
+require('./validMeReact');
